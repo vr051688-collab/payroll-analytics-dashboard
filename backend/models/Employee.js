d3c423cf7158661335c5beb2685bdac973d4997b
vr051688-collab <vr051688@gmail.com>
2026-07-23 23:26:12 +0530

    Add employeeId and department fields to Employee model

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -1,4 +1,8 @@
-const mongoose=require('mongoose');
-module.exports=mongoose.model('Employee',new mongoose.Schema({
- name:String,email:String,salary:Number
+const mongoose = require('mongoose');
+module.exports = mongoose.model('Employee', new mongoose.Schema({
+  name: String,
+  email: String,
+  employeeId: String,
+  department: String,
+  salary: Number
 }));
